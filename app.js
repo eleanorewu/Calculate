3904aca2b40c11f3d4c77c7b938dad761444b21c
uie-823 <yuenwu1996@gms.tku.edu.tw>
2020-03-15 11:56:24 +0800

    JS

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ var result = document.getElementById("result");
 // 取得按鈕：0-9 +-*/
 var btns = document.getElementsByClassName("btn");
 
-console.log(btns);
+// 按鈕[編號].添加事件監聽器("點擊"，匿名函式() { 函式(); })
+btns[0].addEventListener("click", function(){
+    // 顯示(按鈕[編號].內容)
+    show(btns[0].innerHTML);
+});
+
+// 函式 顯示(參數-內容)
+function show(content){
+    // 結果元素.內容 = 參數-內容
+    result.innerHTML = content;
+}
+
