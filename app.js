1ab21c833a9fd591c5030bc77e3754f6834522de
uie-823 <yuenwu1996@gms.tku.edu.tw>
2020-03-15 12:53:44 +0800

    尚未輸入數字避免輸入0*/

--- app.js
+++ app.js
@@ -5,18 +5,45 @@ var btns = document.getElementsByClassName("btn");
 // 最終數字
 var final = "";
 
+// 等於按鈕
+var equalBtn = document.getElementById("equal");
+// 監聽器(""，等於)
+equalBtn.addEventListener("click", equal);
+
+function equal() {
+
+    // 如果 最終數字 等於 "" 就 跳出
+    if (final == "") return;
+
+    // eval 運算字串
+    result.innerHTML = eval(final);
+    // 清空
+    final = "";
+}
+
 // var 在 function 內使用
 // let 在 { } 內使用
-for (let i =0; i <btns.length; i++){
+for (let i = 0; i < btns.length; i++) {
     // 按鈕[編號].添加事件監聽器("點擊"，匿名函式() { 函式(); })
-    btns[i].addEventListener("click", function(){
+    btns[i].addEventListener("click", function() {
+        
         // 顯示(按鈕[編號].內容)
         show(btns[i].innerHTML);
     });
 }
+
 // 函式 顯示(參數-內容)
 function show(content) {
+
+    // 處理尚未輸入數字
+    if (final == "") {
+        if (content == "0") return;
+        if (content == "*") return;
+        if (content == "/") return;
+    }
+
+    // 最終數字 += 參數-內容
     final += content;
-    // 結果元素.內容 = 參數-內容
+    // 結果元素.內容 = 最終數字
     result.innerHTML = final;
 }
