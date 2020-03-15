691548e71e0e6ed46050b6b1e8998aeda2ae1145
uie-823 <yuenwu1996@gms.tku.edu.tw>
2020-03-15 12:12:32 +0800

    增加click

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var result = document.getElementById("result");
 // 取得按鈕：0-9 +-*/
 var btns = document.getElementsByClassName("btn");
 
-// 按鈕[編號].添加事件監聽器("點擊"，匿名函式() { 函式(); })
-btns[0].addEventListener("click", function(){
-    // 顯示(按鈕[編號].內容)
-    show(btns[0].innerHTML);
-});
-
+// var 在 function 內使用
+// let 在 { } 內使用
+for (let i =0; i <btns.length; i++){
+    // 按鈕[編號].添加事件監聽器("點擊"，匿名函式() { 函式(); })
+    btns[i].addEventListener("click", function(){
+        // 顯示(按鈕[編號].內容)
+        show(btns[i].innerHTML);
+    });
+}
 // 函式 顯示(參數-內容)
-function show(content){
+function show(content) {
     // 結果元素.內容 = 參數-內容
     result.innerHTML = content;
 }
-
